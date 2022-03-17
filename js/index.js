8c00f5bd1399a058d418ca77a1247ef046ac3692
Emanuel Rouse <41143874+emanuelrouse@users.noreply.github.com>
2022-03-17 14:51:28 -0400

    mac transfer

---
+++ js/index.js
@@ -0,0 +1,9 @@
+// console.log("connected");
+// function includeProjects(file) {
+//   let script = document.createElement('script');
+//   script.src = file;
+//   script.type = 'text/javascript';
+//   script.defer = true;
+
+//   document.getElementsByTagName('body')
+// }
---
+++ js/projects.js
@@ -0,0 +1,5 @@
+const projects = {
+  name: ["Pokemon-Quiz"],
+  screenshots: [["../assets/screenshots/pokemon-quiz/..."]],
+  skills: [["HTML, CSS, jQuery, Dom Manipulation"]],
+};
